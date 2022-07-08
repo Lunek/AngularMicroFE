e2e6b71303d7dd5efb77f6a2e5a056e2ffc7b4ff
Alexander Bilakovskyi <abi@dbbsoftware.com>
2022-07-08 22:29:59 +0300

    Activated and configured module federation

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,29 +1,30 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'flights',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:3000/remoteEntry.js',
+        exposedModule: './Module',
+        type: 'module',
+      })
+        .then((m) => m.FlightsModule)
+        .catch(() => console.error('Error loading remote module!')),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 
-
-    // Your route here:
-
-    // {
-    //   path: 'flights',
-    //   loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-    // },
-
-    {
-      path: '**',
-      component: NotFoundComponent
-    }
-
-    // DO NOT insert routes after this one.
-    // { path:'**', ...} needs to be the LAST one.
-
+  // DO NOT insert routes after this one.
+  // { path:'**', ...} needs to be the LAST one.
 ];
-
